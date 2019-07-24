bf0d1404c84df52dded3a5d05ff3bb1315ffe263
Aunt Anne <orcasalmoneagle@gmail.com>
2019-07-23 20:36:13 -0700

    Update form.js
    Fix relative path to layout.js

--- src/pages/contact/form.js
+++ src/pages/contact/form.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from "gatsby"
 import { navigate } from 'gatsby-link'
-import Layout from '../components/layout'
+import Layout from '../../components/layout'
 
 function encode(data) {
   return Object.keys(data)
